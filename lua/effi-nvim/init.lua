e06125a77a5b08965cb5b8592f8c244d37e3e8a8
Aino Spring <info@aino-spring.com>
2023-06-22 15:08:28 +0200

    print infos

--- lua/effi-nvim/init.lua
+++ lua/effi-nvim/init.lua
@@ -21,9 +21,11 @@ end
 function M.load()
   ConfigPath = vim.fn.getcwd() .. "/.effi";
   if not file_exists(ConfigPath) then
+    print("Could not load project");
     return
   end
   Data = lip.load(ConfigPath);
+  print("Loaded " .. Data["project"]["name"]);
 end
 
 function M.run()
@@ -38,12 +40,17 @@ function M.run()
       on_stderr = function (jobid, data, event)
         table.insert(Output, data[1]);
       end,
+      on_exit = function (jobid, data, event)
+        print("Exited");
+      end,
     }
   );
+  print("Running");
 end
 
 function M.stop()
   vim.fn.jobstop(Job);
+  print("Exited");
 end
 
 function M.print()
